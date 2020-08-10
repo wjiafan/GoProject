a135d1bc92485827d7be01fc6d78b85ecca94d79
wjiafan <851940496@qq.com>
2020-08-10 18:33:19 +0800

    实现了simple-RS编码和解码的过程

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"./pro_1"
+	"fmt"
+)
 
 /**
  * Created by Administrator on 2020/8/10 0010 下午 15:57
@@ -8,4 +11,8 @@ import "fmt"
 
 func main(){
 	fmt.Println("Hello,world!")
+	//编码
+	pro_1.SimpleEncoder()
+	//解码
+	//pro_1.SimpleDecoder()
 }
---
+++ pro_1/simple-RS.go
@@ -0,0 +1,26 @@
+package pro_1
+
+import (
+	"fmt"
+	"os"
+)
+
+/**
+ * Created by Administrator on 2020/8/10 0010 下午 18:12
+ */
+
+const(
+	dataShards = 4	//数据分片数
+	parShards = 2 	//校验分片数
+)
+
+/**
+func_name: 		checkErr
+func_result: 	success->nil	fail->!nil
+*/
+func checkErr(err error){
+	if err != nil{
+		fmt.Fprint(os.Stderr,"Error:%s",err.Error())
+		os.Exit(2)
+	}
+}
