ffa8a1c0fab7b2a067ea90fbfa17fa4465b4df2a
wjiafan <851940496@qq.com>
2020-08-11 12:29:29 +0800

    【1】实现RS 【2】实现一致性哈希算法

---
+++ pro_2/simple-consistenthash.go
@@ -0,0 +1,5 @@
+package pro_2
+
+/**
+ * Created by Administrator on 2020/8/11 0011 上午 10:22
+ */  
---
+++ pro_2/simple-consistenthash_test.go
@@ -0,0 +1,48 @@
+package pro_2
+
+/**
+ * Created by Administrator on 2020/8/11 0011 上午 10:31
+ */
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestHashing(t *testing.T) {
+
+	hash := New(3, func(key []byte) uint32 {
+		i, _ := strconv.Atoi(string(key))
+		return uint32(i)
+	})
+
+	// Given the above hash function, this will give replicas with "hashes":
+	// 2, 4, 6, 12, 14, 16, 22, 24, 26
+	hash.Add("6", "4", "2")
+
+	testCases := map[string]string{
+		"2":  "2",
+		"11": "2",
+		"23": "4",
+		"27": "2",
+	}
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("Asking for %s, should have yielded %s", k, v)
+		}
+	}
+
+	// Adds 8, 18, 28
+	hash.Add("8")
+
+	// 27 should now map to 8.
+	testCases["27"] = "8"
+
+	for k, v := range testCases {
+		if hash.Get(k) != v {
+			t.Errorf("Asking for %s, should have yielded %s", k, v)
+		}
+	}
+
+}
